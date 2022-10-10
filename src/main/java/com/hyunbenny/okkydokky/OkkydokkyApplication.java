daf658e026d5077319b957a5e6c79dcd17f3f63e
hyunbin <hyunbincho@ezens.net>
2022-10-11 08:46:23 +0900

    initial commit

---
+++ src/main/java/com/hyunbenny/okkydokky/OkkydokkyApplication.java
@@ -0,0 +1,13 @@
+package com.hyunbenny.okkydokky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkkydokkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkkydokkyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyunbenny/okkydokky/OkkydokkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunbenny.okkydokky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkkydokkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
